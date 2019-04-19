29cadcbdcfa44a080d27a2004ce3c4021a75bf8a
RR2511 <ruth.reynders91@gmail.com>
2019-04-19 18:04:04 +0200

    cypress setup

--- apps/rdpizza-e2e/src/integration/app.spec.ts
+++ apps/rdpizza-e2e/src/integration/app.spec.ts
@@ -1,9 +1,19 @@
-import { getGreeting } from '../support/app.po';
+import { getCardHeaders } from '../support/app.po';
 
-describe('Hello Nx', () => {
-  beforeEach(() => cy.visit('/'));
+describe('RD Pizza Service', () => {
+  beforeEach(() => {
+    cy.server();
+    cy.fixture('pizzas.json').as('pizzasJSON');
+    cy.route('GET', '/api/pizzas', '@pizzasJSON').as('getPizzas');
+
+    cy.visit('/products');
+    cy.wait(['@getPizzas']);
+  });
 
   it('should display welcome message', () => {
-    getGreeting().contains('Welcome to rdpizza!');
+    getCardHeaders().should('have.length', 3);
+    getCardHeaders().contains("Blazin' Inferno");
+    getCardHeaders().contains("Seaside Surfin'");
+    getCardHeaders().contains("Blazin' Inferno");
   });
 });
